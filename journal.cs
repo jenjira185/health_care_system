f588ded2860965fafd436a6a071f2ee489bfd20d
jenjira185 <jenjira.h04@gmail.com>
2025-10-13 11:38:14 +0200

    Add a comment
    Finished my journal file
    
    I have been commenting what the codes are doing

--- journal.cs
+++ journal.cs
@@ -1,12 +1,39 @@
 namespace HealthCareSystem;
 
+
+// class for journal with keys
 class journal
 {
-    private string Title;
-    private string Descriptions;
-    private string Results;
+    public string Title;
+    public string Descriptions;
+    public DateTime dateCreated;
+    public DateTime dateLast;
 
-    private int patientID;
-    private int staffID;
-    
+    //using ID so the patient journal can get access to correct personnel
+    public int patientID;
+    public int staffID;
+
+
+
+    // A constructor to create a new journal
+    public journal(string title, string desc, int patID, int inChargeID)
+    {
+        Title = title;
+        Descriptions = desc;
+        patientID = patID;
+        staffID = inChargeID;
+
+        //using datetime to know when the journal have been created
+        dateCreated = DateTime.Now;
+        dateLast = DateTime.Now;
+    }
+
+
+    // An new constructor for the journal to update
+    public void UpdateJournal(string newDesc, int modifierStaffID)
+    {
+        Descriptions = newDesc;
+        staffID = modifierStaffID;
+        dateLast = DateTime.Now;   //using datetime for journal's last update of the patient
+    }
 }
--- location.cs
+++ location.cs
@@ -1,5 +1,3 @@
-using System.Globalization;
-
 namespace HealthCareSystem;
 
 class Location
