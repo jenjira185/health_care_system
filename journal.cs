577eb2b271b5617b5551cb0d756fbe84295eb6a5
jenjira185 <jenjira.h04@gmail.com>
2025-10-13 10:49:44 +0200

    Add a comment
    :

--- journal.cs
+++ journal.cs
@@ -1,2 +1,12 @@
 namespace HealthCareSystem;
 
+class journal
+{
+    private string Title;
+    private string Descriptions;
+    private string Results;
+
+    private int patientID;
+    private int staffID;
+    
+}
