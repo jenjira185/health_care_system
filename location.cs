4fd5b66b89eb354af0814b581cada03315c999b0
jenjira185 <jenjira.h04@gmail.com>
2025-10-16 10:06:21 +0200

    Add a comment
    I have been adding some new methods in my location.
    
    I have made a new file for schedule, but will considering to give the
    schedule codes to Isabella since she is handling with appointments, and
    I think schedule is more connected to appointments.

--- location.cs
+++ location.cs
@@ -6,8 +6,7 @@ class Location
     public string Region;
     public string Adress;
     public string Name;
-
-    public List<string> schedule();
+    public List<string> schedule();     //a string for schedule to be included in location
 
     public Location(string region, string adress, string name)
     {
@@ -15,6 +14,31 @@ class Location
         Region = region;
         Adress = adress;
         Name = name;
-        schedule = new List<string>();
+        schedule = new List<string>();    //Initializing the schedule like an empty list
+    }
+
+    public int GetLocationID()      //Instead of using get; set; I use the method to return the value
+    {
+        return locationID;
+    }
+    public string GetName()
+    {
+        return Name;
+    }
+
+    public List<string> GetSchedule()     //This returns the whole schedule 
+    {
+        return schedule;      
+    }
+
+    public void AddToSchedule(string appointmentDetails)     //This method is used to add an appointment in the schedule
+    {
+        schedule.Add(appointmentDetails);
+    }
+
+    public void UpdateLocationDetails(string newRegion, string newAdress)     //This method lets the Admin to get an updated information
+    {
+        Region = newRegion;
+        Adress = newAdress;
     }
 }
---
+++ schedule.cs
@@ -0,0 +1,25 @@
+namespace HealthCareSystem;
+
+class schedule
+{
+    public List<Appointment> appointments;
+
+    public schedule()
+    {
+        appointments = new List<Appointment>();
+    }
+
+    public void AddAppointment(string details)
+    {
+        appointments.Add(details);
+    }
+
+    public List<string> ViewSchedule()
+    {
+        return new List<string>(appointments);
+    }
+    public void RemoveAppointment(string details)
+    {
+        appointments.Remove(details);
+    }
+}
