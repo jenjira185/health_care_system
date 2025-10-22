20d713934018a20b085d6411a9e0140f033ed048
jenjira185 <jenjira.h04@gmail.com>
2025-10-22 23:23:38 +0200

    Add a comment
    Finished writing Kevin's admin.cs

--- admin.cs
+++ admin.cs
@@ -1,2 +1,78 @@
 namespace HealthCareSystem;
 
+class Admin : User
+{
+    public List<string> Permissions { get; set; }
+    public List<string> Locations { get; set; }
+    public List<Personnel> PersonnelAccounts { get; set; }
+    public List<string> Schedule { get; set; }
+
+    public Admin(string name, string password)
+    : base(name, password)
+    {
+        Permissions = new List<string>();
+        Locations = new List<string>();
+        PersonnelAccounts = new List<Personnel>();
+        Schedule = new List<string>();
+    }
+
+    public void HandlePermissions(string permissions)
+    {
+        if (IsLoggedIn)
+        {
+            Permissions.Add(permission);
+            Console.WriteLine($"Added permission: {permission}");
+        }
+        else
+        {
+            Console.WriteLine("Please log in first");
+        }
+    }
+
+    public void AddLocation(string location)
+    {
+        if (IsLoggedIn)
+        {
+            Locations.Add(location);
+            Console.WriteLine($"Added new location: {location}");
+        }
+        else
+        {
+            Console.WriteLine("Please log in first");
+        }
+    }
+
+    public void CreatePersonnelAccount(string name, string password, string location)
+    {
+        if (IsLoggedIn)
+        {
+            Personnel newPersonnel = new Personnel(name, password, location);
+            PersonnelAccounts.Add(newPersonnel);
+            Console.WriteLine($"Created personnel account: {name} {location}");
+        }
+        else
+        {
+            Console.WriteLine("Please log in first");
+        }
+    }
+
+    public void ViewPermissionsList()
+    {
+        Console.WriteLine("Permissions list: ");
+        if (Permissions.Count == 0)
+            Console.WriteLine("No permissions assigned");
+        else
+            for (int i = 0; i < Permissions.Count; i++)
+                Console.WriteLine($"- {Permissions[i]}");
+    }
+
+    public override void ViewSchedule()
+    {
+        Console.WriteLine($"Admin schedule for {Name}: ");
+        if (Schedule.Count == 0)
+            Console.WriteLine("No scheduled items");
+        else
+            for (int i = 0; i < Schedule.Count; i++)
+                Console.WriteLine($"- {Schedule[i]}");
+    }
+}
